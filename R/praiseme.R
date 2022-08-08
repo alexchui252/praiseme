eedc8587a0daa08a954117d2e246f601ec57aaaf
alexchui252 <alexchui252@gmail.com>
2022-08-08 17:20:20 -0400

    roxygen namespace

--- R/praiseme.R
+++ R/praiseme.R
@@ -1,3 +1,15 @@
+#' Praises user
+#'
+#' \code{praiseme} returns praise for the name given
+#'
+#' @param name user-specified name
+#'
+#' @return a character string
+#' @export
+#'
+#' @examples
+#' praiseme("Alex")
 praiseme <- function(name){
+
     paste0("You are awesome ", name, "!")
 }
